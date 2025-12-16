f7e74d202577970846c603e01ec5a115920e2ad6
Gosh Hernandez <osvaldocube@gmail.com>
2025-12-16 00:43:55 -0600

    feat: IO-000 update to server proxy

--- server/structures/SocketIO.js
+++ server/structures/SocketIO.js
@@ -7,83 +7,71 @@ const { pluginId } = require('../utils/pluginId');
 const { API_TOKEN_TYPE } = require('../utils/constants');
 
 class SocketIO {
-	constructor(options) {
-		this._socket = new Server(strapi.server.httpServer, options);
-		const { hooks } = strapi.config.get(`plugin::${pluginId}`);
-		hooks.init?.({ strapi, $io: this });
-		this._socket.use(handshake);
-	}
+  constructor({ strapi, options }) {
+    // 🔥 clave: asegurarte que Koa confía en headers del proxy
+    if (strapi?.server?.app) {
+      strapi.server.app.proxy = true;
+    }
 
-	// eslint-disable-next-line no-unused-vars
-	async emit({ event, schema, data: rawData }) {
-		const sanitizeService = getService({ name: 'sanitize' });
-		const strategyService = getService({ name: 'strategy' });
-		const transformService = getService({ name: 'transform' });
+    this._strapi = strapi;
+    this._socket = new Server(strapi.server.httpServer, options);
 
-		// account for unsaved single content type being null
-		if (!rawData) {
-			return;
-		}
+    const { hooks } = strapi.config.get(`plugin::${pluginId}`);
+    hooks.init?.({ strapi, $io: this });
 
-		const eventName = `${schema.singularName}:${event}`;
+    this._socket.use(handshake);
+  }
 
-		for (const strategyType in strategyService) {
-			if (Object.hasOwnProperty.call(strategyService, strategyType)) {
-				const strategy = strategyService[strategyType];
+  async emit({ event, schema, data: rawData }) {
+    const strapi = this._strapi;
+    const sanitizeService = getService({ name: 'sanitize' });
+    const strategyService = getService({ name: 'strategy' });
+    const transformService = getService({ name: 'transform' });
 
-				const rooms = await strategy.getRooms();
+    if (!rawData) return;
 
-				for (const room of rooms) {
-					const permissions = room.permissions.map(({ action }) => ({ action }));
-					const ability = await strapi.contentAPI.permissions.engine.generateAbility(permissions);
+    const eventName = `${schema.singularName}:${event}`;
 
-					if (room.type === API_TOKEN_TYPE.FULL_ACCESS || ability.can(schema.uid + '.' + event)) {
-						// sanitize
-						const sanitizedData = await sanitizeService.output({
-							data: rawData,
-							schema,
-							options: {
-								auth: {
-									name: strategy.name,
-									ability,
-									strategy: {
-										verify: strategy.verify,
-									},
-									credentials: strategy.credentials?.(room),
-								},
-							},
-						});
+    for (const strategyType in strategyService) {
+      const strategy = strategyService[strategyType];
+      const rooms = await strategy.getRooms();
 
-						const roomName = strategy.getRoomName(room);
+      for (const room of rooms) {
+        const permissions = room.permissions.map(({ action }) => ({ action }));
+        const ability = await strapi.contentAPI.permissions.engine.generateAbility(permissions);
 
-						// transform
-						const data = transformService.response({ data: sanitizedData, schema });
-						// emit
-						this._socket.to(roomName.replace(' ', '-')).emit(eventName, { ...data });
-					}
-				}
-			}
-		}
-	}
+        if (room.type === API_TOKEN_TYPE.FULL_ACCESS || ability.can(schema.uid + '.' + event)) {
+          const sanitizedData = await sanitizeService.output({
+            data: rawData,
+            schema,
+            options: {
+              auth: {
+                name: strategy.name,
+                ability,
+                strategy: { verify: strategy.verify },
+                credentials: strategy.credentials?.(room),
+              },
+            },
+          });
 
-	async raw({ event, data, rooms }) {
-		let emitter = this._socket;
+          const roomName = strategy.getRoomName(room);
+          const data = transformService.response({ data: sanitizedData, schema });
 
-		// send to all specified rooms
-		if (rooms && rooms.length) {
-			rooms.forEach((r) => {
-				emitter = emitter.to(r);
-			});
-		}
+          this._socket.to(roomName.replace(' ', '-')).emit(eventName, { ...data });
+        }
+      }
+    }
+  }
 
-		emitter.emit(event, { data });
-	}
+  async raw({ event, data, rooms }) {
+    let emitter = this._socket;
+    if (rooms?.length) rooms.forEach((r) => (emitter = emitter.to(r)));
+    emitter.emit(event, { data });
+  }
 
-	get server() {
-		return this._socket;
-	}
+  get server() {
+    return this._socket;
+  }
 }
 
-module.exports = {
-	SocketIO,
-};
+module.exports = { SocketIO };
